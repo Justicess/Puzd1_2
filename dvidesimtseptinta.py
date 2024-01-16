929453ccb993c067daa2bcffa10b7af1a487e689
Justicess <71278409+Justicess@users.noreply.github.com>
2024-01-16 20:54:03 +0200

    Update

--- dvidesimtseptinta.py
+++ dvidesimtseptinta.py
@@ -26,3 +26,34 @@ class Words:
        return cls.revers_string(b)
     
 print(Words.get_reversed_word(input("write word: ")))
+
+
+# Task 3
+class BankAccount:
+
+    total_accounts = 0
+    def __init__(self, owner, balance = 0.0):
+        self.owner = owner
+        self.balance = balance
+        BankAccount.total_accounts +=1
+
+    def show_balance(self):
+        print(f"account name {self.owner}, balance; {self.balance}")
+    @classmethod
+    def get_total_accounts(cls):
+        return cls.total_accounts
+    @staticmethod
+    def validate_amount(amount):
+        if amount<0:
+            return False
+        else:
+            return True
+        
+
+account1 = BankAccount("a",10000)
+account2 = BankAccount("b",20000)
+account1.show_balance()
+account2.show_balance()
+print(BankAccount.get_total_accounts())
+print(BankAccount.validate_amount(100))
+print(BankAccount.validate_amount(-100))
